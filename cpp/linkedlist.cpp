1a0e76e31c45024bdbe2f95a43a54f893e87c864
tinyynoob_home <starcraft2empire@gmail.com>
2021-10-23 12:09:05 +0800

    1023 practice list

---
+++ cpp/linkedlist.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+#include <list>
+#include<iterator>
+using namespace std;
+
+
+
+int main()
+{
+    list <int> myList;
+    list <int>::iterator it = myList.begin();
+    int i,n,data;
+    cout<<"number of elements:";
+    cin>>n;
+    for(i=0;i<n;i++){
+        cout<<i<<"-th data"<<endl;
+        cin>>data;
+        myList.push_back(data);
+    }
+    cout<<"insert at:";
+    cin>>n;
+    cout<<"insert data:";
+    cin>>data;
+    for(i=0;i<n;i++)
+        ++it;
+    myList.insert(it,data);
+    
+    for(it=myList.begin(); it!=myList.end(); ++it)
+        cout<<*it;
+    cout<<endl;
+    system("pause");
+    return 0;
+}
--- test.cpp
+++ test.cpp
@@ -1,7 +1,19 @@
-#include<iostream>
+#include <iostream>
+#include<list>
 using namespace std;
+
+
+
 int main()
 {
-    cout<<"Hello, world!"<<endl;
+    list <int> myList;
+    int i,n,data;
+    cout<<"number of elements:";
+    cin>>n;
+    for(i=0;i<n;i++){
+        cout<<i<<"-th data"<<endl;
+        cin>>data;
+        myList.push_back(data);
+    }
     return 0;
 }
