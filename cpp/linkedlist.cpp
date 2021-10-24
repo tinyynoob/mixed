d8c208a21e6b20548e1d112fcbbe0dbd66f91bb9
tinyynoob_home <starcraft2empire@gmail.com>
2021-10-24 22:54:47 +0800

    add reverse function

--- cpp/linkedlist.cpp
+++ cpp/linkedlist.cpp
@@ -4,6 +4,7 @@
 #include<iterator>
 using namespace std;
 
+list <int> reverse_list(list <int>);
 
 
 int main()
@@ -24,7 +25,7 @@ int main()
 
     do
     {
-        cout<<"choose function: (1)insert_at (0)exit"<<endl<<":";
+        cout<<"choose function: (1)insert_at (2)delete_at (3)list_reverse (0)exit"<<endl<<":";
         cin>>branch;
         switch(branch){
             case 1:
@@ -36,6 +37,21 @@ int main()
                 for(i=0;i<index;i++)
                     it++;
                 myList.insert(it,data);
+                break;
+            case 2:
+                cout<<"delete at:";
+                cin>>index;
+                if(index>=myList.size() || index<0)
+                    cout<<"error index\n";
+                else{
+                    it = myList.begin();
+                    for(i=0;i<index;i++)
+                        it++;
+                    myList.erase(it);
+                }
+                break;
+            case 3:
+                myList = reverse_list(myList);
                 break;
             case 0:
                 break;
@@ -51,4 +67,13 @@ int main()
     cout<<endl;
     system("pause");
     return 0;
+}
+
+
+list <int> reverse_list(list <int> oriList){
+    list <int> tempList;
+    list <int>::iterator it;
+    for(it=oriList.begin(); it!=oriList.end(); it++)
+        tempList.push_front(*it);
+    return tempList;
 }
