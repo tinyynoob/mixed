cd3531de4cb37371bf9a5e414d77a5d35824f91b
tinyynoob_home <starcraft2empire@gmail.com>
2021-10-24 19:55:01 +0800

    myList initial with init.txt and its usage is now based on switch_case

--- cpp/linkedlist.cpp
+++ cpp/linkedlist.cpp
@@ -1,5 +1,6 @@
-#include <iostream>
-#include <list>
+#include<iostream>
+#include<fstream>
+#include<list>
 #include<iterator>
 using namespace std;
 
@@ -9,25 +10,44 @@ int main()
 {
     list <int> myList;
     list <int>::iterator it;
-    int i,n,data;
-    cout<<"number of elements:";
-    cin>>n;
-    for(i=0;i<n;i++){
-        cout<<i<<"-th data"<<endl;
-        cin>>data;
-        myList.push_back(data);
+    ifstream file;
+    int i,index,n,data,branch;
+    file.open("init.txt");
+    if(file.is_open()){
+        file>>n;
+        for(i=0;i<n;i++){
+            file>>data;
+            myList.push_back(data);
+        }
     }
-    cout<<"insert at:";
-    cin>>n;
-    cout<<"insert data:";
-    cin>>data;
-    it = myList.begin();
-    for(i=0;i<n;i++)
-        ++it;
-    myList.insert(it,data);
-    
+    file.close();
+
+    do
+    {
+        cout<<"choose function: (1)insert_at (0)exit"<<endl<<":";
+        cin>>branch;
+        switch(branch){
+            case 1:
+                cout<<"insert at:";
+                cin>>index;
+                cout<<"insert data:";
+                cin>>data;
+                it = myList.begin();
+                for(i=0;i<index;i++)
+                    it++;
+                myList.insert(it,data);
+                break;
+            case 0:
+                break;
+            default:
+                cout<<"not matched option\n";
+                break;
+        }
+    }while(branch);
+
+    cout<<"myList:\n";
     for(it=myList.begin(); it!=myList.end(); ++it)
-        cout<<*it;
+        cout<<*it<<"\t";
     cout<<endl;
     system("pause");
     return 0;
