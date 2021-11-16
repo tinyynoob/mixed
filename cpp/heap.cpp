fed33a1490759d1eca40f28de575135f08df0214
tinyynoob_home <starcraft2empire@gmail.com>
2021-11-16 22:38:34 +0800

    initialize heap.cpp
    in class heap, finish get_root(), getHeight(), delete_root(), and reHeapDown()

---
+++ cpp/heap.cpp
@@ -0,0 +1,89 @@
+#include<iostream>
+#include<fstream>
+#include<cmath>
+#include<vector>
+using namespace std;
+
+#define swap(a,b) {int t; t=a; a=b; b=t;}
+
+class heap  //assume binary min heap
+{
+public:
+    heap(fstream);
+    heap(int []);
+    int get_root();
+    int getHeight();    //notice that single-node tree has height 0
+    void insert(int);
+    int delete_root();  //if no root to delete, return 1; Otherwise, return 0;
+private:
+    vector <int> nodes;
+    //void reHeapUp();
+    void reHeapDown(int);
+};
+
+int main()
+{
+    
+    system("pause");
+    return 0;
+}
+
+int heap::get_root()
+{
+    return nodes.at(0); //.at() includes bounds checking
+}
+
+int heap::getHeight()
+{
+    if(nodes.size()==0)
+        return -1;
+    else
+        return (int)log2(nodes.size());
+}
+
+void heap::insert(int input)
+{
+    nodes.push_back(input);
+    //reHeapUp();
+}
+
+int heap::delete_root()
+{
+    if(getHeight()==-1) //heap is empty, no loop to delete
+        return 1;
+    swap(nodes.at(0), nodes.back());
+    nodes.pop_back();
+    reHeapDown(0);
+    return 0;
+}
+
+
+
+
+void heap::reHeapDown(int root)   //root = root index of the subtree
+{
+    int left, right, key;
+    if(root >= nodes.size())
+        return;
+    key = -1;
+    left = 2*root+1;
+    right = 2*root+2;
+    if(left<=nodes.size()){    //if left subtree exists
+        if(right<=nodes.size()){    //if right subtree exists
+            if(nodes.at(left)<nodes.at(root) && nodes.at(left)<=nodes.at(right))    //if left is least
+                key = left;
+            else if(nodes.at(right)<nodes.at(root) && nodes.at(right)<=nodes.at(left))  //if right is least
+                key = right;
+        }
+        else    //if right subtree does not exist
+            if(nodes.at(left) < nodes.at(root))
+                key = left;
+    }
+    if(key==-1) //if key unchanged
+        return;
+    else{
+        swap(nodes.at(root), nodes.at(key));
+        reHeapDown(key);
+        return;
+    }
+}
