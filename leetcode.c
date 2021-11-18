1ee15eca9a5223ccaff372ac9158817f67754604
tinyynoob_home <starcraft2empire@gmail.com>
2021-11-18 12:59:51 +0800

    devloping  hash version of twoSum

--- leetcode.c
+++ leetcode.c
@@ -2,71 +2,89 @@
 #include<stdlib.h>
 #include<string.h>
 
-#define max(a,b) (a>b)?a:b
+#define INT_MAX 2147483647
 
-char * longestPalindrome(char * s);
+int* twoSum(int* nums, int numsSize, int target, int* returnSize);
+void hash_insert(int *hashTable, int key, const int tableSize);
+int hash_function(int key, const int tableSize);
+int hash_search(int *hashTable, int key, const int tableSize);
 
 int main()
 {
-    char s[10];
-    strcpy(s,"222020221");
-    printf("%s", longestPalindrome(s));
+    int i, *p;
+    int a[10]={3,2,4};
+    twoSum(a, 3, 6, p);
     system("pause");
     return 0;
 }
 
-char * longestPalindrome(char * s){
-    int i, j, offset, length, maxIndex, *LPS;
-    char *extended_s, *ans;
+int* twoSum(int* nums, int numsSize, int target, int* returnSize){
+    int i, j, gap, n, *ans;
+    int *hashTable, tableSize;
     
-    length = 0;
-    for(i=0; s[i]; i++)
-        length++;
-    length = 2*length+1;    //length of extended_s
-    LPS = (int*)malloc(sizeof(int)*(length));
-    extended_s = (char*)malloc(sizeof(char)*(length+1));    //+1 space for '\0'
-    for(i=0; i<length/2; i++){
-        extended_s[2*i] = '_';    //padding, to make even string easy
-        extended_s[2*i+1] = s[i];
-    }
-    extended_s[length-1] = '_';
-    extended_s[length] = '\0';
+    /*---to ensure tableSize = 2^n-1, that is, in this form 00..0011..11----*/
+    tableSize = numsSize<<1;
+    for(i=0; tableSize; tableSize>>=1)
+        i++;
+    tableSize = 1;
+    for(; i; i--)
+        tableSize<<= 1;
+    tableSize--;
     
-    i = 0;
-    while(i<length){    //build LPS table
-        j = 0;  //used as flag
-        offset = 1;
-        while(i-offset>=0 && i+offset<length && extended_s[i-offset]==extended_s[i+offset]) //if not exceed bound and be palindromic
-            offset++;
-        LPS[i] = offset;
-        for(offset=1; i-offset>=0&&offset<LPS[i]; offset++){
-            if(offset+LPS[i-offset] < LPS[i])
-                LPS[i+offset] = LPS[i-offset];
-            else if(offset+LPS[i-offset] > LPS[i])
-                LPS[i+offset] = LPS[i]-offset;
-            else if(offset+LPS[i-offset] == LPS[i]){
-                i+= offset;    //determine the distance to skip
-                j = 1;
-                break;
-            }
+    hashTable = (int*)malloc(sizeof(int)*tableSize);
+    for(i=0; i<tableSize; i++)  //init hashTable
+        hashTable[i] = INT_MAX;
+    
+    for(i=0; i<numsSize; i++){
+        gap = target-nums[i];
+        n = hash_search(hashTable, gap, tableSize);
+        if(n != -1){  //if found
+            ans = (int*)malloc(sizeof(int)*2);
+            for(j=0; nums[j]!=gap; j++)
+                ;
+            ans[0] = j;
+            ans[1] = i;
+            free(hashTable);
+            //*returnSize = 2;
+            return ans;
         }
-        if(!j)
-            i+= max(1, offset-1);   //determine the distance to skip
+        hash_insert(hashTable, nums[i], tableSize); //if num[i] can not form an answer, insert it to hashTable
     }
-    maxIndex = 0;
-    for(i=1; i<length; i++){    //search LPS table to find the position with longest palindrome
-        if(LPS[i] > LPS[maxIndex])
-            maxIndex = i;
+    free(hashTable);
+    //*returnSize = 0;
+    return NULL;
+}
+
+void hash_insert(int *hashTable, int key, const int tableSize){
+    int value;
+    value = hash_function(key, tableSize);
+    while(hashTable[value] != INT_MAX){ //linear probing
+        value++;
+        value = value&tableSize; //modular add
     }
-    ans = (char*)malloc(sizeof(char)*LPS[maxIndex]);
-    j = 0;
-    for(i=maxIndex-LPS[maxIndex]+1; i<maxIndex+LPS[maxIndex]; i++)
-        if(extended_s[i]!='_')
-            ans[j++] = extended_s[i];
-    ans[j] = '\0';
-    free(LPS);
-    free(extended_s);
-    return ans;
+    hashTable[value] = key;
+}
+
+int hash_function(int key, const int size){
+    /* the hash function is from https://ppt.cc/fO5Jrx */
+    if(key<0)
+        key = ~key;
+    key = key ^ (key >> 4);
+    key = (key + (key << 3)) + (key << 7);
+    key = key ^ (key >> 5);
+    return key&size;
+}
+
+int hash_search(int *hashTable, int key, const int tableSize){
+    int value;
+    value = hash_function(key, tableSize);
+    while(hashTable[value]!=key){
+        if(hashTable[value] == INT_MAX) //not found
+            return -1;
+        value++;
+        value = value&tableSize;
+    }
+    return value;
 }
 
 
