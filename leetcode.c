cb8cb4cb8b21ab17c9a3c30078cd7d36113f003d
tinyynoob_home <starcraft2empire@gmail.com>
2021-11-18 14:03:08 +0800

    finish the rewrite of leetcode 1

--- leetcode.c
+++ leetcode.c
@@ -2,90 +2,14 @@
 #include<stdlib.h>
 #include<string.h>
 
-#define INT_MAX 2147483647
 
-int* twoSum(int* nums, int numsSize, int target, int* returnSize);
-void hash_insert(int *hashTable, int key, const int tableSize);
-int hash_function(int key, const int tableSize);
-int hash_search(int *hashTable, int key, const int tableSize);
 
 int main()
 {
     int i, *p;
     int a[10]={3,2,4};
-    twoSum(a, 3, 6, p);
+    
     system("pause");
     return 0;
 }
-
-int* twoSum(int* nums, int numsSize, int target, int* returnSize){
-    int i, j, gap, n, *ans;
-    int *hashTable, tableSize;
-    
-    /*---to ensure tableSize = 2^n-1, that is, in this form 00..0011..11----*/
-    tableSize = numsSize<<1;
-    for(i=0; tableSize; tableSize>>=1)
-        i++;
-    tableSize = 1;
-    for(; i; i--)
-        tableSize<<= 1;
-    tableSize--;
-    
-    hashTable = (int*)malloc(sizeof(int)*tableSize);
-    for(i=0; i<tableSize; i++)  //init hashTable
-        hashTable[i] = INT_MAX;
-    
-    for(i=0; i<numsSize; i++){
-        gap = target-nums[i];
-        n = hash_search(hashTable, gap, tableSize);
-        if(n != -1){  //if found
-            ans = (int*)malloc(sizeof(int)*2);
-            for(j=0; nums[j]!=gap; j++)
-                ;
-            ans[0] = j;
-            ans[1] = i;
-            free(hashTable);
-            //*returnSize = 2;
-            return ans;
-        }
-        hash_insert(hashTable, nums[i], tableSize); //if num[i] can not form an answer, insert it to hashTable
-    }
-    free(hashTable);
-    //*returnSize = 0;
-    return NULL;
-}
-
-void hash_insert(int *hashTable, int key, const int tableSize){
-    int value;
-    value = hash_function(key, tableSize);
-    while(hashTable[value] != INT_MAX){ //linear probing
-        value++;
-        value = value&tableSize; //modular add
-    }
-    hashTable[value] = key;
-}
-
-int hash_function(int key, const int size){
-    /* the hash function is from https://ppt.cc/fO5Jrx */
-    if(key<0)
-        key = ~key;
-    key = key ^ (key >> 4);
-    key = (key + (key << 3)) + (key << 7);
-    key = key ^ (key >> 5);
-    return key&size;
-}
-
-int hash_search(int *hashTable, int key, const int tableSize){
-    int value;
-    value = hash_function(key, tableSize);
-    while(hashTable[value]!=key){
-        if(hashTable[value] == INT_MAX) //not found
-            return -1;
-        value++;
-        value = value&tableSize;
-    }
-    return value;
-}
-
-
 
--- leetcode/1.c
+++ leetcode/1.c
@@ -1,74 +1,91 @@
 
-void hash_insert(int *hashTable, int key, const int tableSize);
+#define INT_MAX 2147483647
+
+struct hashTable{
+    int *table;
+    int size;
+};
+
+void hash_insert(struct hashTable *hash, const int key);
 int hash_function(int key, const int tableSize);
-int hash_search(int *hashTable, int key, const int tableSize);
+int hash_search(const struct hashTable *hash, const int key);
+void hash_clear(struct hashTable *hash);
 
 /**
  * Note: The returned array must be malloced, assume caller calls free().
  */
 int* twoSum(int* nums, int numsSize, int target, int* returnSize){
-    int i, j, gap, value, *ans;
-    int *hashTable, tableSize;
-    
-    tableSize = numsSize<<1;
-    for(i=0; tableSize; tableSize>>1)
+    int i, j, toFind, n, *ans;
+    struct hashTable *hash;
+    hash = (struct hashTable*)malloc(sizeof(struct hashTable));
+    hash->table = NULL;
+    /*---to ensure tableSize = 2^k, that is, in this binary form 00..0010..00----*/
+    n = numsSize<<1;
+    for(i=0; n; n>>=1)
         i++;
-    tableSize = 1;
+    n = 1;
     for(; i; i--)
-        tableSize<<1;
-        
-    hashTable = (int*)malloc(sizeof(int)*tableSize);
-    for(i=0; i<tableSize; i++)
-        hashTable[i] = -1;
-    for(i=0; i<numsSize; i++)
-        hash_insert(hashTable, nums[i], tableSize);
+        n<<= 1;
+    hash->size = n;
+    /*----end set tableSize----*/
+    hash->table = (int*)malloc(sizeof(int)*hash->size);
+    for(i=0; i<hash->size; i++)  //initialize all the value in hashTable to INT_MAX
+        hash->table[i] = INT_MAX;
     
     for(i=0; i<numsSize; i++){
-        gap = target-nums[i];
-        value = hash_function(gap, tableSize);
-        if(hashTable[value]!=-1){
-            for(j=0; j<numsSize; j++){
-                if(nums[j]==gap && j!=i){
-                    ans = (int*)malloc(sizeof(int)*2);
-                    ans[0] = i;
-                    ans[1] = j;
-                    free(hashTable);
-                    *returnSize = 2;
-                    return ans;
-                }
-            }
+        toFind = target-nums[i];
+        if(hash_search(hash, toFind) != -1){  //if found answer
+            ans = (int*)malloc(sizeof(int)*2);
+            j = 0;
+            while(nums[j] != toFind)    //find the number's index in nums[]
+                j++;
+            ans[0] = j;
+            ans[1] = i;
+            hash_clear(hash);
+            *returnSize = 2;
+            return ans;
         }
+        hash_insert(hash, nums[i]); //if the numbers in hashTable can not form an answer with nums[i], insert num[i] to hashTable
     }
-    free(hashTable);
+    hash_clear(hash);
     *returnSize = 0;
     return NULL;
 }
 
-void hash_insert(int *hashTable, int key, const int tableSize){
+void hash_insert(struct hashTable *hash, const int key){
     int value;
-    value = hash_function(key, tableSize);
-    while(hashTable[value]!=-1) //keep hashing to find an empty position
-        value = hash_function(value, tableSize);
-    hashTable[value] = key;
+    value = hash_function(key, hash->size);
+    while(hash->table[value] != INT_MAX){   //linear probing (modular addition)
+        value++;
+        value = value&((hash->size)-1);
+    }
+    hash->table[value] = key;
 }
 
 int hash_function(int key, const int size){
-    /* the hash function is from https://ppt.cc/fO5Jrx */
-
-    return (key*7)%size;
+    if(key<0)
+        key = ~key;
+    key = key ^ (key >> 4);
+    key = ~key + (key << 3);
+    key = key ^ (key >> 7);
+    return key&(size-1);    //since size-1=00...011...11, the bit operation & is equivalent to %
 }
 
-int hash_search(int *hashTable, int key, const int tableSize){
+int hash_search(const struct hashTable *hash, const int key){   //if found, return its index in hashTable
     int value;
-    value = hash_function(key, tableSize);
-    if(hashTable[value]==-1)    //not found
-        return -1;
-    else{
-        while(hashTable[value]!=key)
-            value = hash_function(value, tableSize);
-        return value;
+    value = hash_function(key, hash->size);
+    while(hash->table[value]!=key){
+        if(hash->table[value] == INT_MAX) //means that it is not in the table 
+            return -1;
+        value++;           //linear probing (modular addition)
+        value = value&((hash->size)-1);
     }
-    return -1; //useless
+    return value;
+}
+
+void hash_clear(struct hashTable *hash){
+    free(hash->table);
+    free(hash);
 }
 
 
