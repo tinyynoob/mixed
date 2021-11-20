57da2aedef21f1ca976f58de68a98fda7f289d84
tinyynoob_home <starcraft2empire@gmail.com>
2021-11-20 16:20:19 +0800

    improve leetcode 540

--- leetcode/540.c
+++ leetcode/540.c
@@ -1,34 +1,17 @@
 
+/*-- according to https://leetcode.com/problems/single-element-in-a-sorted-array/discuss/1587588 --*/
 
 int singleNonDuplicate(int* nums, int numsSize){
-    int left, right, index, type;
-    if(numsSize == 1)
-        return nums[0];
+    int left, right, index;
     left = 0;
-    right = numsSize-1;
-    while(right-left > 2){
+    right = numsSize-2;
+    while(left <= right){
         index = (left+right)/2;
-        type = index&1; //mod 2
-        if((type==0&&nums[index]==nums[index-1])||(type==1&&nums[index]==nums[index+1]))
-            right = index;  //go left
-        else if((type==0&&nums[index]==nums[index+1])||(type==1&&nums[index]==nums[index-1]))
-            left = index;   //go right
+        if(nums[index] == nums[index^1])
+            left = index+1;   //go right
         else
-            return nums[index];    //the answer is found
+            right = index-1;  //go left
     }
-    
-    if(right-left == 2){    // 3 numbers remain
-        if(nums[left] == nums[left+1])
-            return nums[right];
-        else if(nums[right] == nums[right-1])
-            return nums[left];
-        else
-            return nums[left+1];
-    }
-    //2 numbers remain. this case only happens at boundary.
-    else if(left==0)    //the two leftmost numbers remain
-        return nums[left];  //it must not be nums[left+1], or there will be two answer
-    else                //the two rightmost numbers remain
-        return nums[right];
+    return nums[left];
 }
 
