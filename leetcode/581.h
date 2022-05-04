1411f27929b2daabdecc44dfb8dcc756102efebf
tinyynoob <sadpotato1920@gmail.com>
2022-05-04 23:28:41 +0800

    Clean code of leetcode 581
    Learn this brilliant method from the Net.

--- leetcode/581.h
+++ leetcode/581.h
@@ -1,61 +1,23 @@
-//#include <stdio.h>
 
 
 int findUnsortedSubarray(int* nums, int numsSize)
 {
-    int stop_increasing = -1;   // from left to right
-    int min = nums[0];
+    int right = -2;
+    int max = nums[0];
     for (int i = 1; i < numsSize; i++) {
-        if (nums[i] <= min)
-            min = nums[i];
-        if (nums[i] < nums[i - 1] && stop_increasing == -1)
-            stop_increasing = i;
-    }
-    //printf("stop_increasing is nums[%d] = %d\n", stop_increasing, nums[stop_increasing]);
-    if (stop_increasing == -1)
-        return 0;
-    int rmin = nums[stop_increasing];
-    for (int i = stop_increasing + 1; i < numsSize; i++) {
-        if (nums[i] < rmin)
-            rmin = nums[i];
-    }
-    int left = -1;
-    if (min == nums[0]) {
-        left = 0;
-        for (int i = stop_increasing - 1; i >= 0; i--) {
-            if (nums[i] <= rmin) {
-                left = i;
-                //printf("left = %d\n", left);
-                break;
-            }
-        }
-    }
-
-    int stop_decreasing = -1;   // from right to left
-    int max = nums[numsSize - 1];
-    for (int i = numsSize - 2; i >= 0; i--) {
         if (nums[i] >= max)
             max = nums[i];
-        if (nums[i] > nums[i + 1] && stop_decreasing == -1)
-            stop_decreasing = i;
+        else
+            right = i;
     }
-    //printf("stop_decreasing is nums[%d] = %d\n", stop_decreasing, nums[stop_decreasing]);
-    // stop_decreasing is impossible to be -1
-    int lmax = nums[stop_decreasing];
-    for (int i = stop_decreasing - 1; i >= 0; i--) {
-        if (nums[i] > lmax)
-            lmax = nums[i];
+    int left = -1;
+    int min = nums[numsSize - 1];
+    for (int i = numsSize - 2; i >= 0; i--) {
+        if (nums[i] <= min)
+            min = nums[i];
+        else
+            left = i;
     }
-    int right = numsSize;
-    if (max == nums[numsSize - 1]) {
-        right = numsSize - 1;
-        for (int i = stop_decreasing + 1; i < numsSize; i++) {
-            if (nums[i] >= lmax) {
-                right = i;
-                //printf("right = %d\n", right);
-                break;
-            }
-        }
-    }
-    return right - left - 1;
+    // if one of left or right is changed, another must also be changed.
+    return right - left + 1;
 }
