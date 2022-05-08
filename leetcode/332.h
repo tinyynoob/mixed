7c8ffc04d67a57e1b998f7451074f347f731bcd3
tinyynoob <sadpotato1920@gmail.com>
2022-05-08 11:28:44 +0800

    Improve 332 by eliminating an if condition
    Since the possible value of needed coins would never be amount + 1, we
    can initialize table[curr] as amount + 1, and it helps eliminate an if
    condition in the nested loop.

--- leetcode/332.h
+++ leetcode/332.h
@@ -5,15 +5,13 @@ int coinChange(int* coins, int coinsSize, int amount)
     int table[amount + 1];
     table[0] = 0;
     for (int curr = 1; curr < amount + 1; curr++) {
-        table[curr] = -1;
+        table[curr] = amount + 1;   // means impossible
         for (int i = 0; i < coinsSize; i++) {
             if (curr - coins[i] < 0)
                 continue;
-            else if (table[curr - coins[i]] == -1)
-                continue;
-            else if (table[curr] == -1 || table[curr - coins[i]] + 1 < table[curr])
+            else if (table[curr - coins[i]] + 1 < table[curr])
                 table[curr] = table[curr - coins[i]] + 1;
         }
     }
-    return table[amount];
+    return (table[amount] == amount + 1) ? -1 : table[amount];
 }
