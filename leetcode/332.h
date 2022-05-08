e5ed516787966d508c196c1aeefe6cb69e73bdd4
tinyynoob <sadpotato1920@gmail.com>
2022-05-08 11:08:37 +0800

    Space improvement from naive
    NOT ACCEPTED version.
    Cannot pass if there is huge number in coins.
    Use ring buffer to construct the table. This version would save much
    storage if values in coins are small and amount is big.

--- leetcode/332.h
+++ leetcode/332.h
@@ -1,19 +1,49 @@
+#include <stdio.h>
+#include <stdint.h>
 
+
+static inline int ilog32(uint32_t v)
+{
+    uint32_t ans = v > 0;
+    int m = (v > 0xFFFFU) << 4;
+    v >>= m;
+    ans |= m;
+    m = (v > 0xFFU) << 3;
+    v >>= m;
+    ans |= m;
+    m = (v > 0xFU) << 2;
+    v >>= m;
+    ans |= m;
+    m = (v > 0x3u) << 1;
+    v >>= m;
+    ans |= m;
+    ans += (v > 1);
+    return ans;
+}
 
 int coinChange(int* coins, int coinsSize, int amount)
 {
-    int table[amount + 1];
+    int max = 0;    // max value among the coins
+    for (int i = 0; i < coinsSize; i++)
+        if (coins[i] > max)
+            max = coins[i];
+    const unsigned size = 1u << ilog32(max);
+    int table[size];
     table[0] = 0;
-    for (int curr = 1; curr < amount + 1; curr++) {
-        table[curr] = -1;
+    for (unsigned curr = 1; curr < amount + 1; curr++) {
+        const unsigned curridx = curr & (size - 1);
+        table[curridx] = -1;
         for (int i = 0; i < coinsSize; i++) {
-            if (curr - coins[i] < 0)
+            const unsigned preidx = (curr - coins[i]) & (size - 1);
+            if ((int) curr - coins[i] < 0)
                 continue;
-            else if (table[curr - coins[i]] == -1)
+            else if (table[preidx] == -1)
                 continue;
-            else if (table[curr] == -1 || table[curr - coins[i]] + 1 < table[curr])
-                table[curr] = table[curr - coins[i]] + 1;
+            else if (table[curridx] == -1)
+                table[curridx] = table[preidx] + 1;
+            else if (table[preidx] + 1 < table[curridx])
+                table[curridx] = table[preidx] + 1;
         }
     }
-    return table[amount];
+    return table[amount & (size - 1)];
 }
