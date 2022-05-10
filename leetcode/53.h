03e1995df2583ddd3462554c4227c9e112b7bd3d
tinyynoob <sadpotato1920@gmail.com>
2022-05-10 10:52:00 +0800

    Shrink the dp table to reduce space complexity

--- leetcode/53.h
+++ leetcode/53.h
@@ -1,15 +1,15 @@
 
-#define max(a,b) ((a > b) ? (a) : (b))
+#define max(a, b) ((a > b) ? (a) : (b))
 
 
 int maxSubArray(int* nums, int numsSize){
-    int dp[numsSize];
+    int dp[2];
     dp[0] = nums[0];
     int mmax = dp[0];
     for (int i = 1; i < numsSize; i++) {
-        dp[i] = max(nums[i], nums[i] + dp[i - 1]);
-        if (dp[i] > mmax)
-            mmax = dp[i];
+        dp[i & 1] = max(nums[i], nums[i] + dp[(i & 1) ^ 1]);
+        if (dp[i & 1] > mmax)
+            mmax = dp[i & 1];
     }
     return mmax;
 }
