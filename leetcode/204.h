635b3b576e796f8bf967dbd54ebda796a5f72da6
tinyynoob <sadpotato1920@gmail.com>
2022-05-18 20:49:21 +0800

    Shrink table size and introduce sqrt()
    Cut table size half by not recording any even number.
    
    Use sqrt() to shrink the range of sieve.

--- leetcode/204.h
+++ leetcode/204.h
@@ -4,25 +4,66 @@
 
 #include <limits.h>
 
-int countPrimes(int n)
+int ilog(uint32_t v)
 {
-    if (n <= 1)
+    int ret = v > 0;
+    int m = (v > (uint32_t) 0xFFFFU) << 4;
+    v >>= m;
+    ret |= m;
+    m = (v > 0xFFU) << 3;
+    v >>= m;
+    ret |= m;
+    m = (v > 0xFU) << 2;
+    v >>= m;
+    ret |= m;
+    m = (v > 0x3U) << 1;
+    v >>= m;
+    ret |= m;
+    ret += (v > 1);
+    return ret - 1;
+}
+
+int mySqrt(int x)
+{
+    uint32_t v = x;
+    if (!v)
         return 0;
-    int ans = 0;
-    bool *mark = (bool *) calloc(sizeof(bool), n);
-    mark[0] = true;
-    mark[1] = true;
-    for (int i = 2; i < n;) {
-        mark[i] = true;
-        if (i < INT_MAX / i + 1) { // to prevent overflow
-            for (int j = i; i * j < n; j++)
-                mark[i * j] = true;
+    int shift = (ilog(v) & ~1u);
+    v -= (uint32_t) 1u << shift;
+    int ans = 1;
+    while (shift > 0) {
+        shift -= 2;
+        uint32_t sub = ((ans << 2) | 1) << shift;
+        if (v >= sub) {
+            v -= sub;
+            ans = (ans << 1) + 1;
+        } else {
+            ans = ans << 1;
         }
-        ans++;
-        while (i < n && mark[i])  // find next
-            i++;
     }
-    free(mark);
     return ans;
 }
 
+int countPrimes(int n)
+{
+    if (n <= 2)
+        return 0;
+    int ans = 1;    // prime 2
+    bool *mark = (bool *) calloc(sizeof(bool), n >> 1); // build table only for odd numbers
+    mark[0] = true;
+    for (int i = 3; i <= mySqrt(n);) {
+        mark[i >> 1] = true;
+        if (i < INT_MAX / i + 1) { // to prevent overflow
+            for (int j = i; i * j < n; j += 2)
+                mark[(i * j) >> 1] = true;
+        }
+        ans++;
+        while (i <= mySqrt(n) && mark[i >> 1])  // find next
+            i += 2;
+    }
+    for (int i = (mySqrt(n) >> 1); i < (n >> 1); i++)
+        if (!mark[i])
+            ans++;
+    free(mark);
+    return ans;
+}
