2d9cd6f63a4ba043789ffcb9c60f095dcf256679
tinyynoob <sadpotato1920@gmail.com>
2022-05-27 14:59:58 +0800

    Provide branchless version of leetcode 231

--- leetcode/231.h
+++ leetcode/231.h
@@ -1,6 +1,9 @@
+#include <stdbool.h>
 
 
 bool isPowerOfTwo(int n)
 {
-    return n && !(n >> 31) && !(n & (n - 1));
+    unsigned u = n;
+    // need to block 0 and INT_MIN, which is 100...00
+    return !(u & (u - 1)) & !(u >> 31) & !((u - 1) >> 31);
 }
---
+++ leetcode/231.s
@@ -0,0 +1,40 @@
+	.file	"231.c"
+	.text
+	.globl	isPowerOfTwo
+	.type	isPowerOfTwo, @function
+isPowerOfTwo:
+.LFB0:
+	.cfi_startproc
+	endbr64
+	leal	-1(%rdi), %edx
+	testl	%edi, %edx
+	sete	%al
+	notl	%edx
+	shrl	$31, %edx
+	andl	%eax, %edx
+	notl	%edi
+	shrl	$31, %edi
+	movl	%edx, %eax
+	andl	%edi, %eax
+	ret
+	.cfi_endproc
+.LFE0:
+	.size	isPowerOfTwo, .-isPowerOfTwo
+	.ident	"GCC: (Ubuntu 9.4.0-1ubuntu1~20.04.1) 9.4.0"
+	.section	.note.GNU-stack,"",@progbits
+	.section	.note.gnu.property,"a"
+	.align 8
+	.long	 1f - 0f
+	.long	 4f - 1f
+	.long	 5
+0:
+	.string	 "GNU"
+1:
+	.align 8
+	.long	 0xc0000002
+	.long	 3f - 2f
+2:
+	.long	 0x3
+3:
+	.align 8
+4:
