3c4215de0dafcdcaa35a0925cae0cb2868f5e46b
tinyynoob <sadpotato1920@gmail.com>
2023-02-25 18:15:30 +0800

    Improve leecode 1442 with the characteristic of XOR
    If a (i, k) pair is valid, then @j can be any value in (i, k].

--- leetcode/1442.c
+++ leetcode/1442.c
@@ -3,16 +3,11 @@ int countTriplets(int* arr, int arrSize)
 {
     int ans = 0;
     for (int i = 0; i < arrSize - 1; i++) {
-        int a = arr[i];
-        for (int j = i + 1; j < arrSize; j++) {
-            int b = arr[j];
-            for (int k = j; k < arrSize; k++) {
-                if (a == b)
-                    ans++;
-                if (k + 1 < arrSize)
-                    b ^= arr[k + 1];
-            }
-            a ^= arr[j];
+        int axorb = arr[i];
+        for (int k = i + 1; k < arrSize; k++) {
+            axorb ^= arr[k];
+            if (axorb == 0)   // implies a == b
+                ans += k - i;
         }
     }
     return ans;
